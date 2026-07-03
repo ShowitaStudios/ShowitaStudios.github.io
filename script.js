1a7504149ab90a20bf92368ef43358dcd5135d47
SeahSehita <127892440+SeahSehita@users.noreply.github.com>
2026-07-03 14:20:05 -0400

    Main Files

---
+++ script.js
@@ -0,0 +1,11 @@
+// Buscamos nuestro botón de acción radical
+const botonGrind = document.getElementById('btnMusica');
+
+// Añadimos interacción al hacer click
+botonGrind.addEventListener('click', () => {
+    // Genera un sonido de alerta del navegador clásico
+    alert("⚡ [SISTEMA EN LÍNEA] BIENVENIDO AL COMPLEJO INDIE ⚡");
+    
+    // Idea para el futuro: Aquí podrás añadir código para reproducir
+    // un bucle de música Breakbeat/Hip-hop de fondo para tu portafolio.
+});
