b1f5308680394f070fa99d11a8f3ffe563260b89
Sehita <127892440+ShowitaStudios@users.noreply.github.com>
2026-07-03 15:17:24 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const botonGrind = document.getElementById('btnMusica');
 // Añadimos interacción al hacer click
 botonGrind.addEventListener('click', () => {
     // Genera un sonido de alerta del navegador clásico
-    alert("⚡ [SISTEMA EN LÍNEA] BIENVENIDO AL COMPLEJO INDIE ⚡");
+    alert("ME PICAN LOS COCOS AYYY");
     
     // Idea para el futuro: Aquí podrás añadir código para reproducir
     // un bucle de música Breakbeat/Hip-hop de fondo para tu portafolio.
